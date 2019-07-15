bf42bcc88cd0aa701556e234524567c334398c65
SophikoTsiklauri <46166586+SophikoTsiklauri@users.noreply.github.com>
2019-07-16 02:17:34 +0400

    new

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,18 @@
-// var next=document.querySelector('.next');
-// var video=document.querySelectorAll('.video');
-// var current_video=document.querySelector('.current_video');
-// var videolength=video.length;
-// next.addEventListener("click", function nextvideo() {
-// 	for(i=0;i<videolength;i++){
-// 		current_video.classList.add('video');
-// 		video[i].classList.add('current_video');
-// 		break;
-// 	}
-  
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
